acb4c9893e24a12504a8c1a7fe19f6582bf395db
LiveG <hi@liveg.tech>
2018-10-14 11:51:30 +0100

    Fix this scope

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $(function() {
                     $(thisPassOn).html("Could not load associated information.");
                 }
             }).done(function(data) {
-                $(this).html(new showdown.Converter().makeHtml(data));
+                $(thisPassOn).html(new showdown.Converter().makeHtml(data));
             });
         } else {
             $(this).html("<em>Content will go here at run-time: " + $(this).attr("markdown") + "</em>");
