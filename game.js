140b47541a4096d01d3aee53996756aca65e5600
James <jamesliveg@gmail.com>
2018-11-01 19:04:14 +0000

    Add SWF functionality (yay)

--- game.js
+++ game.js
@@ -22,5 +22,27 @@ $(function() {
         $(".creatorAccountName").text(gameData.by);
 
         refreshCreatorPpic();
+
+        if (gameData.src != undefined && gameData.src.endsWith(".swf")) {
+            $("#gameFrame").html(`
+                <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="100%" height="100%">
+                    <param name="movie" value="` + gameData.src + `" />
+                    <param name="quality" value="high" />
+                    <param name="scale" value="default" />
+                    <embed src="` + gameData.src + `" quality="high" type="application/x-shockwave-flash" width="100%" height="100%" scale="default" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+                </object>
+            `);
+        } else {
+            $("#gameFrame").html(`
+                <h3>The file that the creator has used a file format that we don't accept.</h3>
+                <p>You may want to leave a comment for the game creator below.</p>
+                <button onclick="window.open('help/index.html?article=0001-supportedFileTypes');">Learn More...</button>
+            `);
+        }
+
+        setInterval(function() {
+            $("#gameLoader").hide();
+            $("#gameFrame").show();
+        }, 2000);
     });
 });
