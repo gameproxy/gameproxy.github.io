c55488af998e53dd00d77675707b32444b795641
James <jamesliveg@gmail.com>
2018-11-02 10:34:41 +0000

    Force HTTP to be HTTPS

--- game.js
+++ game.js
@@ -26,10 +26,10 @@ $(function() {
         if (gameData.src != undefined && gameData.src.endsWith(".swf")) {
             $("#gameFrame").html(`
                 <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="100%" height="100%">
-                    <param name="movie" value="` + gameData.src + `" />
+                    <param name="movie" value="` + gameData.src.replace(/http:\/\//g, "https://") + `" />
                     <param name="quality" value="high" />
                     <param name="scale" value="default" />
-                    <embed src="` + gameData.src + `" quality="high" type="application/x-shockwave-flash" width="100%" height="100%" scale="default" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+                    <embed src="` + gameData.src.replace(/http:\/\//g, "https://") + `" quality="high" type="application/x-shockwave-flash" width="100%" height="100%" scale="default" pluginspage="http://www.macromedia.com/go/getflashplayer" />
                 </object>
             `);
         } else {
