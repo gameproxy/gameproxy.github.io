016f0701f8c6c50d0e062eddbd92a2196804b2d4
James <jamesliveg@gmail.com>
2018-11-10 11:42:01 +0000

    Fix that homescreen thing

--- countdownTest.js
+++ countdownTest.js
@@ -1,3 +1,7 @@
+function getURLParameter(name) {
+    return decodeURIComponent((new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.search) || [null, ""])[1].replace(/\+/g, "%20")) || null;
+}
+
 firebase.auth().onAuthStateChanged(function() {
     var countdownDate = new Date("Nov 24, 2018 16:00:00").getTime();
     var now = new Date().getTime();
