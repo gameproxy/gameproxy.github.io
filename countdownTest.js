26919b47cfb54dd944eaa5c8f9cb41ec1c0bbeff
James <jamesliveg@gmail.com>
2018-11-10 11:29:38 +0000

    Add countdown redirect to all pages

--- countdownTest.js
+++ countdownTest.js
@@ -1,15 +1,10 @@
-function getURLParameter(name) {
-    return decodeURIComponent((new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.search) || [null, ""])[1].replace(/\+/g, "%20")) || null;
-}
-
 firebase.auth().onAuthStateChanged(function() {
     var countdownDate = new Date("Nov 24, 2018 16:00:00").getTime();
     var now = new Date().getTime();
     var distance = countdownDate - now;
 
     if (distance > 0) {
-        // Debug purposes only
-        if (!isStaff(currentUid) && getURLParameter("doCountdown") == "true") {
+        if (!isStaff(currentUid)) {
             window.location.href = "countdown.html";
         }
 }});
