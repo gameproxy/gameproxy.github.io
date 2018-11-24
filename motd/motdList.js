e294ab4a49e21c54e318aa1259e470fe6811c607
James-Livesey <jamesliveg@gmail.com>
2018-11-24 14:41:17 +0000

    Preparing for release

--- motd/motdList.js
+++ motd/motdList.js
@@ -5,11 +5,11 @@ var motdList = [
         "onclick": "window.location.href = 'about.html';"
     },
 
-    {
-        "src": "motd/inDevelopment.png",
-        "alt": "We'll be creating a vast library of games very soon! We hope to see you when we've finished our website. In the meantime, stick around to see what we're doing!",
-        "onclick": "window.open('https://github.com/gameproxy/gameproxy.github.io');"
-    },
+    // {
+    //     "src": "motd/inDevelopment.png",
+    //     "alt": "We'll be creating a vast library of games very soon! We hope to see you when we've finished our website. In the meantime, stick around to see what we're doing!",
+    //     "onclick": "window.open('https://github.com/gameproxy/gameproxy.github.io');"
+    // },
 
     {
         "src": "motd/verified.png",
