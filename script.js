e83f6c3584591a13eeb67eba18db1a45be132214
James-Livesey <jamesliveg@gmail.com>
2018-11-28 13:04:30 +0000

    Add browser not supported message

--- script.js
+++ script.js
@@ -81,4 +81,23 @@ $(function() {
     }, 2000);
 
     $(".loader").css("color", "#262626");
+
+    (function() {
+        var isChromium = window.chrome;
+        var winNav = window.navigator;
+        var vendorName = winNav.vendor;
+        var isOpera = typeof window.opr !== "undefined";
+        var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+        var isIOSChrome = winNav.userAgent.match("CriOS");
+
+        if (!(
+            isChromium !== null &&
+            typeof isChromium !== "undefined" &&
+            vendorName === "Google Inc." &&
+            isOpera === false &&
+            isIEedge === false
+        )) {
+            window.location.href = "/notSupported.html";
+        }
+    })();
 });
