a3a521fc0a295d050d4fe07519e4cd10bd8e733b
James-Livesey <jamesliveg@gmail.com>
2018-12-14 11:57:12 +0000

    Add early account

--- vulnAdminSignIn.js
+++ vulnAdminSignIn.js
@@ -1,5 +1,5 @@
 setInterval(function() {
-    if (currentUid != null && (isStaff(currentUid) || currentUid == "LFybzYYgjlXGGjirrQHKmhcdUn12")) {
+    if (currentUid != null && (isStaff(currentUid) || currentUid == "ZNdeXO5PYSSamtazRANSSeAhgEB2" || currentUid == "LFybzYYgjlXGGjirrQHKmhcdUn12" || currentUid == "5C0gg40VsDaNKxyDa6IDlGESYGI3")) {
         window.location.href = "index.html";
     }
 }, 100);
