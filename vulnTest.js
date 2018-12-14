2fa9d1ebc1bb4053b056f2f317a1addcdbc317fd
James-Livesey <jamesliveg@gmail.com>
2018-12-14 11:51:53 +0000

    Add early access account

--- vulnTest.js
+++ vulnTest.js
@@ -1,5 +1,5 @@
 firebase.auth().onAuthStateChanged(function() {
-    if (!isStaff(currentUid) && currentUid != "ZNdeXO5PYSSamtazRANSSeAhgEB2" && currentUid != "LFybzYYgjlXGGjirrQHKmhcdUn12") {
+    if (!isStaff(currentUid) && currentUid != "ZNdeXO5PYSSamtazRANSSeAhgEB2" && currentUid != "LFybzYYgjlXGGjirrQHKmhcdUn12" && currentUid != "5C0gg40VsDaNKxyDa6IDlGESYGI3") {
         window.location.href = "vuln.html";
     }
 });
