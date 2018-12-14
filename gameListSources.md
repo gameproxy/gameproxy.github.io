a935302bb8deab0e26632ce9ecf95a653718a195
sebastiandoe5 <39373619+sebastiandoe5@users.noreply.github.com>
2018-12-14 18:01:41 +0000

    Help James!

---
+++ gameListSources.md
@@ -0,0 +1,4 @@
+## Sources for lists of games in SWF and embeddable ones
+
+* (Lee Reilly GitHub Games list)[https://github.com/leereilly/games]
+* 
--- motd/motdList.js
+++ motd/motdList.js
@@ -5,11 +5,11 @@ var motdList = [
         "onclick": "window.location.href = 'about.html';"
     },
 
-    // {
-    //     "src": "motd/inDevelopment.png",
-    //     "alt": "We'll be creating a vast library of games very soon! We hope to see you when we've finished our website. In the meantime, stick around to see what we're doing!",
-    //     "onclick": "window.open('https://github.com/gameproxy/gameproxy.github.io');"
-    // },
+    {
+        "src": "motd/GameProxyChoice/TheHouse.png",
+        "alt": "GameProxy Choice: The House",
+        "onclick": "window.location.href = 'https://gameproxy.host/game.html?play=-LTh3lHod5qJ7qZwGXQV';"
+    },
 
     {
         "src": "motd/verified.png",
