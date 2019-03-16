afba678d89456bf3627c4eee84195557705d8bd4
Sebastian Doe <sebastiandoe5@gmail.com>
2019-03-16 07:47:05 +0000

    Add Copy Current URL function to game.html

--- game.js
+++ game.js
@@ -688,3 +688,14 @@ $("#commentBox").keypress(function(e) {
         postComment();
     }
 });
+
+function copyCurrentURL() {
+    var dummy = document.createElement('input'),
+    text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
