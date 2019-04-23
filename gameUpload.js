6f8ce4c572952bfd7f2a7ce6cd043b986bc999e6
James-Livesey <jamesliveg@gmail.com>
2019-04-23 13:01:14 +0100

    Add validator for non-HTTPS links

--- gameUpload.js
+++ gameUpload.js
@@ -34,49 +34,56 @@ function formatDate(date) {
 }
 
 function gameUpload() {
-    if (!uploadingGame) {
-        uploadingGame = true;
+    if ($("#gameLink").val().startsWith("http://") || $("#gameLink").val().startsWith("https://")) {
+        if (!uploadingGame) {
+            uploadingGame = true;
 
-        $(".uploadGame").text("Uploading...");
-        $(".uploadGame").css({
-            backgroundColor: "#7e7e7e",
-            color: "black",
-            cursor: "default"
-        });
+            $(".uploadGame").text("Uploading...");
+            $(".uploadGame").css({
+                backgroundColor: "#7e7e7e",
+                color: "black",
+                cursor: "default"
+            });
 
-        firebase.database().ref("users/" + currentUid + "/_settings/name").once("value", function(snapshot) {
-            var name = snapshot.val();
+            firebase.database().ref("users/" + currentUid + "/_settings/name").once("value", function(snapshot) {
+                var name = snapshot.val();
 
-            toDataUrl(
-                $("#gameLink").val().startsWith("https://scratch.mit.edu/projects/")?
-                    "https://cors-anywhere.herokuapp.com/" + "https://cdn2.scratch.mit.edu/get_image/project/" + $("#gameLink").val().split("/")[4] + "_288x216.png"
-                :   (
-                        $("#gameThumbnail").val() != "" ?
-                            "https://cors-anywhere.herokuapp.com/" + $("#gameThumbnail").val()
-                        :   null
-                    )
-                ,
-                function(base64Img) {
-                    var newGame = firebase.database().ref("games").push()
-                    
-                    newGame.set({
-                        title: profanity.clean($("#gameTitle").val()),
-                        thumbnail: base64Img,
-                        src: $("#gameLink").val().replace(/http:\/\//g, "https://"),
-                        description: profanity.clean($("#gameDescription").val()),
-                        metrics: {likes: 0},
-                        dateAdded: formatDate(new Date()),
-                        category: $("#gameTag").val(),
-                        uid: currentUid,
-                        by: name,
-                        byStaff: isStaff(currentUid),
-                        verified: isStaff(currentUid)
-                    }).then(function() {
-                        console.log(newGame);
-                        window.location.href = "game.html?play=" + newGame.key;
-                    });
-                }
-            );
-        });
+                toDataUrl(
+                    $("#gameLink").val().startsWith("https://scratch.mit.edu/projects/")?
+                        "https://cors-anywhere.herokuapp.com/" + "https://cdn2.scratch.mit.edu/get_image/project/" + $("#gameLink").val().split("/")[4] + "_288x216.png"
+                    :   (
+                            $("#gameThumbnail").val() != "" ?
+                                "https://cors-anywhere.herokuapp.com/" + $("#gameThumbnail").val()
+                            :   null
+                        )
+                    ,
+                    function(base64Img) {
+                        var newGame = firebase.database().ref("games").push()
+                        
+                        newGame.set({
+                            title: profanity.clean($("#gameTitle").val()),
+                            thumbnail: base64Img,
+                            src: $("#gameLink").val().replace(/http:\/\//g, "https://"),
+                            description: profanity.clean($("#gameDescription").val()),
+                            metrics: {likes: 0},
+                            dateAdded: formatDate(new Date()),
+                            category: $("#gameTag").val(),
+                            uid: currentUid,
+                            by: name,
+                            byStaff: isStaff(currentUid),
+                            verified: isStaff(currentUid)
+                        }).then(function() {
+                            console.log(newGame);
+                            window.location.href = "game.html?play=" + newGame.key;
+                        });
+                    }
+                );
+            });
+        }
+    } else {
+        dialog("Please check your info", `
+            In the Game File Link section, make sure that you add a <code>https://</code>
+            at the start to validate the link!
+        `, [{text: "OK", onclick: "closeDialog();", type: "primary"}])
     }
 }
