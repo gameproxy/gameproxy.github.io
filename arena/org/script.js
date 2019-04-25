1a3bfaf3714212b4c5ebd8dcd31fb721b6dc8798
James-Livesey <jamesliveg@gmail.com>
2019-04-25 18:36:31 +0100

    Add checking for computer existence before committing action

--- arena/org/script.js
+++ arena/org/script.js
@@ -18,7 +18,19 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 function shHoldingScreen(screens, show = true) {
     for (var i = 0; i < screens.length; i++) {
-        firebase.database().ref("arena/computers/" + screens[i].trim() + "/holdingScreen").set(show);
+        var screenPassOn = screens[i];
+
+        function passOnValue(value) {
+            firebase.database().ref("arena/computers/" + screens[i].trim() + "/holdingScreen").once("value", function(snapshot) {
+                if (snapshot.val() == null) {
+                    alert("One or more computers do not exist in the action list. The action may have completed on other computers.", "Error while setting option");
+                } else {
+                    firebase.database().ref("arena/computers/" + value.trim() + "/holdingScreen").set(show);
+                }
+            });
+        }
+
+        passOnValue(screens[i]);
     }
 }
 
