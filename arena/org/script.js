3ae4f1c52f315a4bb4ca4a7fc84871304c3d2ab2
James-Livesey <jamesliveg@gmail.com>
2019-04-26 13:39:41 +0100

    Add option to delete computers from the face of the internet

--- arena/org/script.js
+++ arena/org/script.js
@@ -34,6 +34,24 @@ function shHoldingScreen(screens, show = true) {
     }
 }
 
+function delComputers(screens) {
+    for (var i = 0; i < screens.length; i++) {
+        var screenPassOn = screens[i];
+
+        function passOnValue(value) {
+            firebase.database().ref("arena/computers/" + screens[i].trim() + "/holdingScreen").once("value", function(snapshot) {
+                if (snapshot.val() == null) {
+                    alert("One or more computers do not exist in the action list. The action may have completed on other computers.", "Error while setting option");
+                } else {
+                    firebase.database().ref("arena/computers/" + value.trim()).set(null);
+                }
+            });
+        }
+
+        passOnValue(screens[i]);
+    }
+}
+
 function shHSAll(show = true) {
     firebase.database().ref("arena/computers").once("value", function(snapshot) {
         var computerList = snapshot.val();
@@ -46,4 +64,18 @@ function shHSAll(show = true) {
 
 function shHSFromTextarea(show = true) {
     shHoldingScreen($("#shHoldingScreens").val().split(","), show);
+}
+
+function delAll() {
+    firebase.database().ref("arena/computers").once("value", function(snapshot) {
+        var computerList = snapshot.val();
+
+        for (var i = 0; i < Object.keys(computerList).length; i++) {
+            delComputers(Object.keys(computerList)[i]);
+        }
+    });
+}
+
+function delFromTextarea() {
+    delComputers($("#shHoldingScreens").val().split(","));
 }
