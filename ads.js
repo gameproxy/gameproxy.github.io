feb19cbd08b6f30d8bb73b9872fab89f794ac60f
James-Livesey <jamesliveg@gmail.com>
2019-10-13 13:04:14 +0100

    Remove random debug line

--- ads.js
+++ ads.js
@@ -3,8 +3,6 @@ var currentAd = 0;
 function loadAd() {
     currentAd = random(0, adsList.length - 1);
 
-    console.log(currentAd);
-
     $(".ad").attr("src", adsList[currentAd]["src"]);
     $(".ad").attr("alt", adsList[currentAd]["alt"]);
     $(".ad").attr("onclick", adsList[currentAd]["onclick"]);
