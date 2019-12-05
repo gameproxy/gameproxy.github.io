78155fc7ef502f9def01dfbd8678437ff36aef00
Sebastian Doe <sebastiandoe5@gmail.com>
2019-12-05 17:17:18 +0000

    Redefine Service Worker support

--- script.js
+++ script.js
@@ -247,14 +247,12 @@ btnAdd.addEventListener('click', (e) => {
       });
 });
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      .then(function(registration) {
+            console.log('Service Worker Registered');
       });
+    navigator.serviceWorker.ready.then(function(registration) {
+       console.log('Service Worker Ready');
     });
   }
