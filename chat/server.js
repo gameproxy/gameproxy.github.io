d2400e5b3836138d4a440ef244c53045ebbb2ead
James-Livesey <jamesliveg@gmail.com>
2020-03-20 13:46:17 +0000

    Add mentioning mechanics

--- chat/server.js
+++ chat/server.js
@@ -5,6 +5,9 @@ var currentChannel = {
     messagesReference: null
 };
 
+var serverOwners = {};
+var serverMembers = {};
+
 function padDigits(digits) {
     return digits < 10 ? "0" + digits : String(digits);
 }
@@ -34,11 +37,68 @@ function toggleChannelList() {
     }
 }
 
+function addText(text) {
+    if (document.selection) {
+        $("#chatBox").focus();
+
+        selection = document.selection.createRange();
+        selection.text = text;
+    } else if ($("#chatBox")[0].selectionStart || $("#chatBox")[0].selectionStart == 0) {
+        var start = $("#chatBox")[0].selectionStart;
+        var end = $("#chatBox")[0].selectionEnd;
+
+        $("#chatBox").val($("#chatBox").val().substring(0, start) + text + $("#chatBox").val().substring(end, $("#chatBox").val().length));
+
+        $("#chatBox").focus();
+
+        $("textarea")[0].setSelectionRange(start + text.length, start + text.length);
+    } else {
+        $("#chatBox").val($("#chatBox").val() + text);
+    }
+}
+
+function addMention(uid) {
+    firebase.database().ref("users/" + uid + "/_settings/name").on("value", function(nameSnapshot) {
+        addText("{" + nameSnapshot.val() + "}");
+    });
+
+    if ($(window).width() <= 600) {
+        $(".channelList").fadeOut();
+        $(".chatContainer, .chatBoxContainer").fadeIn();
+
+        $(".channelDropdownIcon").text("arrow_drop_down");
+    }
+}
+
+function getUsersByWildcardName(name) {
+    var filtered = [];
+
+    var nameWithoutPrefix = name.split(":").length > 1 ? name.split(":")[1] : name;
+
+    if (name.startsWith("owners:") || name.split(":").length == 1) {
+        for (var key in serverOwners) {
+            if (new RegExp("^" + nameWithoutPrefix.replace(/\*/g, ".*") + "$").test(serverOwners[key])) {
+                filtered.push(key);
+            }
+        }
+    }
+
+    if (name.startsWith("member:") || name.split(":").length == 1) {
+        for (var key in serverMembers) {
+            if (new RegExp("^" + nameWithoutPrefix.replace(/\*/g, ".*") + "$").test(serverMembers[key])) {
+                filtered.push(key);
+            }
+        }
+    }
+
+    return filtered;
+}
+
 function addMessage(message) {
     var converter = new showdown.Converter();
 
     var usernameLink = $("<a target='_blank'>")
-        .attr("href", "https://gameproxy.host/profile.html?user=" + encodeURIComponent(message.uid))
+        .attr("href", "/profile.html?user=" + encodeURIComponent(message.uid))
         .append(
             $("<strong>")
                 .text(profanity.clean(message.by))
@@ -49,6 +109,8 @@ function addMessage(message) {
 
     if (message.byStaff) {
         usernameLink.find("strong").css("color", "#27ef70");
+    } else if (Object.keys(serverOwners).indexOf(message.uid) > -1) {
+        usernameLink.find("strong").css("color", "#42aaf5");
     } else if (isGameProxyPro(message.uid)) {
         usernameLink.find("strong").css("color", "#b3c20f");
     }
@@ -90,6 +152,13 @@ function switchChannel(channelKey) {
     currentChannel.reference.once("value", function() {
         $(".chatContainer").scrollTop($(".chatContainer")[0].scrollHeight);
     });
+
+    if ($(window).width() <= 600) {
+        $(".channelList").fadeOut();
+        $(".chatContainer, .chatBoxContainer").fadeIn();
+
+        $(".channelDropdownIcon").text("arrow_drop_down");
+    }
 }
 
 function postChatMessage() {
@@ -105,7 +174,24 @@ function postChatMessage() {
                 content: profanity.clean($("#chatBox").val())
             });
             
-            // TODO: Add user mentioning to send notifications
+            var userMatches = $("#chatBox").val().match(/\{(.*?)\}/g);
+            var usersMentioned = [];
+
+            if (userMatches != null) {
+                for (var i = 0; i < userMatches.length; i++) {
+                    if (userMatches[i] != "{}") {
+                        usersMentioned.push(...getUsersByWildcardName(userMatches[i].replace(/\{/g, "").replace(/\}/g, "")));
+                    }
+                }
+            }
+
+            for (var i = 0; i < usersMentioned.length; i++) {
+                firebase.database().ref("users/" + usersMentioned[i] + "/chat/mentions").push().set({
+                    from: currentUid,
+                    fromuser: name,
+                    message: profanity.clean($("#chatBox").val())
+                });
+            }
 
             $("#chatBox").val("");
         });
@@ -142,6 +228,84 @@ $(function() {
 
                         $(".channelButton[data-key='" + currentChannel.key + "']").addClass("selected");
                     });
+                })(key);
+            }
+        });
+
+        firebase.database().ref("chat/servers/" + getURLParameter("server") + "/owners").on("value", function(snapshot) {
+            $(".owners").html("");
+
+            firebase.database().ref("chat/servers/" + getURLParameter("server") + "/masterowner").once("value", function(masterownerSnapshot) {                
+                $(".owners").append(
+                    $("<a class='channelButton'>")
+                        .attr("data-owner", masterownerSnapshot.val())
+                        .attr("href", "javascript:addMention('" + masterownerSnapshot.val() + "')")
+                );
+                
+                firebase.database().ref("users/" + masterownerSnapshot.val() + "/_settings/name").on("value", function(nameSnapshot) {
+                    $(".channelButton[data-owner='" + masterownerSnapshot.val() + "']").text(nameSnapshot.val());
+
+                    serverOwners[masterownerSnapshot.val()] = nameSnapshot.val();
+                });
+
+                if (isStaff(masterownerSnapshot.val())) {
+                    $(".channelButton[data-owner='" + masterownerSnapshot.val() + "']").css("color", "#27ef70");
+                } else { // Skipping Pro as ownership of server is more significant
+                    $(".channelButton[data-owner='" + masterownerSnapshot.val() + "']").css("color", "#42aaf5");
+                }
+            });
+            
+            for (var key in snapshot.val()) {
+                (function(key) {
+                    $(".owners").append(
+                        $("<a class='channelButton'>")
+                            .attr("data-owner", key)
+                            .attr("href", "javascript:addMention('" + key + "')")
+                    );
+
+                    firebase.database().ref("users/" + key + "/_settings/name").on("value", function(nameSnapshot) {
+                        $(".channelButton[data-owner='" + key + "']").text(nameSnapshot.val());
+
+                        serverOwners[key] = nameSNapshot.val();
+                    });
+
+                    if (isStaff(key)) {
+                        $(".channelButton[data-owner='" + key + "']").css("color", "#27ef70");
+                    } else { // Skipping Pro as ownership of server is more significant
+                        $(".channelButton[data-owner='" + key + "']").css("color", "#42aaf5");
+                    }
+                })(key);
+            }
+        });
+
+        firebase.database().ref("chat/servers/" + getURLParameter("server") + "/members").on("value", function(snapshot) {
+            $(".members").html("");
+
+            if (snapshot.val() == null) {
+                $(".membersHeader").hide();
+            } else {
+                $(".membersHeader").show();
+            }
+            
+            for (var key in snapshot.val()) {
+                (function(key) {
+                    $(".members").append(
+                        $("<a class='channelButton'>")
+                            .attr("data-member", key)
+                            .attr("href", "javascript:addMention('" + key + "')")
+                    );
+
+                    firebase.database().ref("users/" + key + "/_settings/name").on("value", function(nameSnapshot) {
+                        $(".channelButton[data-member='" + key + "']").text(nameSnapshot.val());
+
+                        serverMembers[key] = nameSnapshot.val();
+                    });
+
+                    if (isStaff(key)) {
+                        $(".channelButton[data-member='" + key + "']").css("color", "#27ef70");
+                    } else if (isGameProxyPro(key)) {
+                        $(".channelButton[data-member='" + key + "']").css("color", "#b3c20f");
+                    }
                 })(key);
             }
         });
