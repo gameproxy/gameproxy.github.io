e0c161681c5d1f5bc1615a1759a5279e54a1a773
James-Livesey <jamesliveg@gmail.com>
2020-03-22 17:36:28 +0000

    Add promos for GameProxy Chat

--- motd/motdList.js
+++ motd/motdList.js
@@ -6,6 +6,12 @@ var motdList = [
     },
 
     {
+        "src": "motd/Chat.png",
+        "alt": "Chat on our brand new online gaming community and create your own server for free today!",
+        "onclick": "window.location.href = '/chat.html';"
+    },
+
+    {
         "src": "motd/Pro.png",
         "alt": "Get GameProxy Pro today! Gain amazing benefits and unlock superpowers on the site!",
         "onclick": "window.location.href = '/pro';"
@@ -13,7 +19,7 @@ var motdList = [
 
     {
         "src": "motd/Merch.png",
-        "alt": "GameProxy Merch",
+        "alt": "We have merch! We deliver to nearly every country in the world, and have stickers, clothing, cases and much more!",
         "onclick": "window.location.href = 'https://www.redbubble.com/people/sebastiandoe5/works/35716450-gameproxy-official-merchandise?asc=u&ref=recent-owner';"
     },
 
@@ -26,18 +32,18 @@ var motdList = [
     {
         "src": "motd/Verified.png",
         "alt": "If a game doesn't have a verified symbol, it may contain extreme violence, offensive material or inappropriate material.",
-        "onclick": "window.location.href = 'report.html';"
+        "onclick": "window.location.href = '/report.html';"
     },
 
     {
         "src": "motd/Staff1.png",
         "alt": "If the creator has a coloured name, it means it was officially uploaded by a staff member of GameProxy.",
-        "onclick": "window.location.href = 'about.html';"
+        "onclick": "window.location.href = '/about.html';"
     },
 
     {
         "src": "motd/Staff2.png",
         "alt": "If the creator has a coloured name, it means it was officially uploaded by a staff member of GameProxy.",
-        "onclick": "window.location.href = 'about.html';"
+        "onclick": "window.location.href = '/about.html';"
     }
 ];
