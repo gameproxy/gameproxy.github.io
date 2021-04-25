e0064651a5d7cfc9724db1866b15d5c7506f2c3a
James-Livesey <jamesliveg@gmail.com>
2021-04-25 11:29:16 +0100

    Remove old vuln pages

--- vulnAdminSignIn.js
+++
@@ -1,5 +0,0 @@
-setInterval(function() {
-    if (currentUid != null && (isStaff(currentUid) || currentUid == "ZNdeXO5PYSSamtazRANSSeAhgEB2" || currentUid == "LFybzYYgjlXGGjirrQHKmhcdUn12" || currentUid == "5C0gg40VsDaNKxyDa6IDlGESYGI3")) {
-        window.location.href = "/";
-    }
-}, 100);
--- vulnTest.js
+++
@@ -1,5 +0,0 @@
-firebase.auth().onAuthStateChanged(function() {
-    if (!isStaff(currentUid) && currentUid != "ZNdeXO5PYSSamtazRANSSeAhgEB2" && currentUid != "LFybzYYgjlXGGjirrQHKmhcdUn12" && currentUid != "5C0gg40VsDaNKxyDa6IDlGESYGI3") {
-        window.location.href = "vuln.html";
-    }
-});
